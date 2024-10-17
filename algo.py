5442169a44117242bf587f64fb549a1f72aea05e
revandry11 <aldeena209@gmail.com>
2024-10-17 20:42:05 +0700

    perubahan jam 8

--- algo.py
+++ algo.py
@@ -1,2 +1,25 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget,QLabel, QHBoxLayout, QVBoxLayout
+
+app = QApplication([])
+
+my_win = QWidget()
+
+my_win.setWindowTitle('My first App')
+my_win.move(900,400)
+my_win.resize(700,400)
+
+
+layout1 = QVBoxLayout()
+
+label1 = QLabel('Hello Guys!')
+label2 = QLabel('My name is Revan')
+layout1.addWidget(label1,alignment=Qt.AlignCenter)
+layout1.addWidget(label2,alignment=Qt.AlignCenter)
+
+
+my_win.setLayout(layout1)
+my_win.show()
+
+app.exec_()
+
